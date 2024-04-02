4c10823a97beef3cddc5a8bd1d8242da8e75508f
katonishields01 <shieldstoni134@gmail.com>
2024-04-02 02:07:11 -0500

    chg

--- HMS/Main/src/Main.java
+++ HMS/Main/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         {
             Scanner scanner = new Scanner(System.in); 
             int choice;
+            int input;
             do {
                 System.out.print("Please Select: ");
                 System.out.println("\n1. Log-In");
@@ -22,9 +23,39 @@ public class Main {
                     case 2:
                         System.out.println("Creating New Account");
                         // Add New Account
+                        do {
+                            System.out.print("Please Select: ");
+                            System.out.println("\n1. Patient");
+                            System.out.println("2. Staff");
+                            System.out.println("3. Exit");
+                            input = scanner.nextInt();
+                        switch (input) {
+                            case 1:
+                             //code   
+                            break;
+                            case 2:
+                            System.out.println("Please Log Admin or Contact Admin to Continue.");
+                            Main in2 = new Main();
+                            in2.Home();   
+                            break;
+                            case 3:
+                            System.out.println("Exiting...");
+                            Main in3 = new Main();
+                            in3.Home();
+                            break;
+                            default:
+                            System.out.println("Invalid option. Please choose a number from 1 to 3.");
+                            Main in4 = new Main();
+                            in4.Home();
+                            break;
+                        }
+                            choice = scanner.nextInt();
+                        }while(input != 3);
                         break;
                     default:
-                        System.out.println("Invalid option. Please choose a number from 1 to 2.");
+                        System.out.println("Invalid option. Please choose a number from 1 to 3.");
+                        Main in5 = new Main();
+                        in5.Home();
                         break;
                 }
                 scanner.close();
